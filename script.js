56aba60bded9f9e49c10695554ff08f5f4ad96fd
Harmony <47876447+timenyin@users.noreply.github.com>
2023-11-04 22:54:26 +0100

    next video 4

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+'use strict';
+/* ======
+//     for textcontent ====
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number ✈️';
+console.log(document.querySelector('.message').textContent);
+
+console.log(document.querySelector('.score').textContent);
+document.querySelector('.score').textContent = '🌟 Recored';
+console.log(document.querySelector('.score').textContent);
+
+// Get the values ===
+console.log((document.querySelector('.guess').value = 23));
+
+*/
+
+// ========= even list
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //   const guess = (document.querySelector('.message').textContent =
+  //     'Correct Number ✈️');
+  console.log(guess, typeof guess);
+  //   console.log(valuescore);
+  if (!guess) {
+    document.querySelector('.message').textContent = '🔐 no number inputed';
+  }
+});
